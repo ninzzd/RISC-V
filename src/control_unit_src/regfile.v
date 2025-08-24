cf604f47ca7db1bb9993c98334a537bc1583ac97
ninzzd <ninaad.desai1212@gmail.com>
2025-08-25 03:18:57 +0530

    Modified the wallace tree code with simple visualization Matches most wallace tree implementations for less than 10-bits Failed for 16-bits as there were 3 partial product bits in the 31st weighted column Included the pc register in the reg_file

--- src/control_unit_src/regfile.v
+++ src/control_unit_src/regfile.v
@@ -1,7 +1,11 @@
 module regfile(
-
+    input wr_en,
+    input [4:0] wr_addr,
+    input [31:0] wr_data,
+    output pc
 );
     reg [31:0] registers [0:31];
+    reg pc;
     always @(*)
     begin
         registers[0] <= 32'h00000000;
