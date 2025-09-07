d03318fd7921eebb6fc471c5318e985b6d4cec79
ninzzd <ninaad.desai1212@gmail.com>
2025-09-08 03:07:24 +0530

    Finished all stages of multiplier Completed the testbench 4 bits in the output (hi) are having undefined values, but the rest is right (must debug)

---
+++ testbench/alu_tb/mul_tb.v
@@ -0,0 +1,20 @@
+`timescale 1ns/1ps
+module mul_tb();
+    reg [31:0] a;
+    reg [31:0] b;
+    wire [63:0] res;
+    mul32 uut(
+        .a(a),
+        .b(b),
+        .lo(res[31:0]),
+        .hi(res[63:32])
+    );
+    initial
+    begin
+        $timeformat(-9,2," ns",6);
+        $monitor("Time = %t, a = %b, b = %b, res = %b",$realtime,a,b,res);
+        #10.000
+        a <= 6;
+        b <= 7;
+    end
+endmodule
