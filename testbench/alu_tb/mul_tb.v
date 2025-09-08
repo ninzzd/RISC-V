eb840f05352aec8ed64b4318f850ecf443604ac9
ninzzd <ninaad.desai1212@gmail.com>
2025-09-08 19:10:16 +0530

    Updated mul32 and mul_tb Dadda multiplier implementation finally successful Typo graphic error (repreated weight 45 twice and skipped 44) in the repeated full adder reduction chains, in stages 4,5,6

--- testbench/alu_tb/mul_tb.v
+++ testbench/alu_tb/mul_tb.v
@@ -3,12 +3,14 @@ module mul_tb();
     reg [31:0] a;
     reg [31:0] b;
     wire [63:0] res;
-    mul32 uut(
+    wire [63:0] exp_res;
+    mul32 #(.T(0.150)) uut(
         .a(a),
         .b(b),
         .lo(res[31:0]),
         .hi(res[63:32])
     );
+    assign exp_res = a*b;
     // task display;
     // endtask
     integer w;
@@ -16,27 +18,37 @@ module mul_tb();
     initial
     begin
         $timeformat(-9,2," ns",6);
-        $monitor("Time = %t, a = %b, b = %b, res = %b",$realtime,a,b,res);
+        $monitor("Time = %t, a = %d, b = %d, exp_res = %d, res = %d",$realtime,a,b,exp_res,res);
         #10.000
-        a <= 2;
-        b <= 3;
+        a <= 292;
+        b <= 6785;
         #1.000
-        // Error in partial product generation (stage 0)
-        // For weight w = 32,33, 'X' is appearing near the base of the column
-        $write("\n");
-        for(w = 62;w >= 0;w = w-1)
-        begin
-            $write("%02d ",w);
-        end
-        $write("\n");
-        for(i = 0; i < 32; i = i+1)
-        begin
-            for(w = 62;w >= 0;w = w-1)
-            begin
-                $write("%2b ",uut.s1[w][i]);
-            end
-            $write("\n");
-        end
+        // ----- Stage-wise debugging -----
+        // $write("\n");
+        // for(w = 63;w >= 0;w = w-1)
+        // begin
+        //     if(w == 63)
+        //     begin
+        //         $write("   ");
+        //     end
+        //     else
+        //     $write("%02d ",w);
+        // end
+        // $write("\n");
+        // for(i = 0; i < 32; i = i+1)
+        // begin
+        //     for(w = 63;w >= 0;w = w-1)
+        //     begin
+        //         if(w == 63)
+        //         begin
+        //             $write("%2d ",i);
+        //         end
+        //         else
+        //         $write("%2b ",uut.s5[w][i]);
+        //     end
+        //     $write("\n");
+        // end
+        // --------------------------------
         #10.000
         $finish;
     end
