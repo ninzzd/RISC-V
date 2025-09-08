1d29fbadb892fec38cb43d3402021806bf54d64e
ninzzd <ninaad.desai1212@gmail.com>
2025-09-08 15:33:58 +0530

    Debugging mul32 Error in stage 0 (partial product generation) One of the partial products is X, somewhere near the base at weights 32, 33

--- testbench/alu_tb/mul_tb.v
+++ testbench/alu_tb/mul_tb.v
@@ -9,16 +9,34 @@ module mul_tb();
         .lo(res[31:0]),
         .hi(res[63:32])
     );
+    // task display;
+    // endtask
+    integer w;
+    integer i;
     initial
     begin
         $timeformat(-9,2," ns",6);
         $monitor("Time = %t, a = %b, b = %b, res = %b",$realtime,a,b,res);
         #10.000
-        a <= 2;
-        b <= 3;
-        #10.000
-        a <= 5;
-        b <= 6;
+        a <= 1;
+        b <= 1;
+        #1.000
+        // Error in partial product generation (stage 0)
+        // For weight w = 32,33, 'X' is appearing near the base of the column
+        $write("\n");
+        for(w = 62;w >= 0;w = w-1)
+        begin
+            $write("%02d ",w);
+        end
+        $write("\n");
+        for(i = 0; i < 32; i = i+1)
+        begin
+            for(w = 62;w >= 0;w = w-1)
+            begin
+                $write("%2b ",uut.s0[w][i]);
+            end
+            $write("\n");
+        end
         #10.000
         $finish;
     end
