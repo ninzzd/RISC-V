fc55df891f53709a1e1dc719d994e9a60fb75ce7
ninzzd <ninaad.desai1212@gmail.com>
2025-09-30 20:40:06 +0530

    Implemented l1_data_cache (behavioral)

--- src/datapath/mem_src/l1_data_cache.v
+++ src/datapath/mem_src/l1_data_cache.v
@@ -1,4 +1,21 @@
-module l1_data_cache(
+ /*
+    Author: Ninaad Desai
+    Description: Simple, behavioral implementation of the L1 Data Cache (inferred BRAM). Single read and write lines with common address decoder
+ */
+module #(parameter N = 1024) l1_data_cache(
+    input clk,          // Clock
+    input [31:0] a,     // Address
+    input [31:0] wd,    // Write-data
+    input we,           // Write-enable
 
+    output [31:0] rd    // Read-data
 );
+    reg [31:0] mem [0:N-1]; // Memory block (Cache)
+    always @(posedge clk)
+    begin
+        if(we)
+            rd <= mem[a];
+        else
+            mem[a] <= wd;
+    end
 endmodule 
--- src/datapath/mem_src/regfile_int.v
+++ src/datapath/mem_src/regfile_int.v
@@ -7,12 +7,12 @@ module regfile_int(
     input [31:0] ra1,       // Read-address 1
     input [31:0] ra2,       // Read-address 2
     input [31:0] wa1,       // Write-address 1
-    input [31:0] dw1,       // Data-write 1
-    input we,               // Write-enable
+    input [31:0] wd1,       // Write-data 1
+    input we,               // Write-enable (LOGIC HIGH)
     input [31:0] pcnext,    // Program-counter next
 
-    output reg [31:0] dr1,  // Data-read 1
-    output reg [31:0] dr2,  // Data-read 2
+    output reg [31:0] rd1,  // Read-data 1
+    output reg [31:0] rd2,  // Read-data 2
     output reg [31:0] pc    // Program-counter
 );
     reg [31:0] x[0:31];     // 32-bit registers (x32)
@@ -22,7 +22,7 @@ module regfile_int(
     begin
         if(we)
         begin
-            x[wa1] <= dw1;
+            x[wa1] <= wd1;
         end
         pc <= pcnext;
     end
@@ -32,8 +32,8 @@ module regfile_int(
         if(~we || (wa1 != ra1 && wa1 != ra2)) // To prevent race conditions when trying to write to and read from the same address
         begin
             // Separate address decoders for dr1 and dr2
-            dr1 <= x[ra1]; 
-            dr2 <= x[ra2];
+            rd1 <= x[ra1]; 
+            rd2 <= x[ra2];
         end
     end
 endmodule
