fecb3da6ab90d00e798490cb2f768a49e582cc22
ninzzd <ninaad.desai1212@gmail.com>
2025-12-19 00:14:17 +0530

    initial datapath (with only pcadd4 and instruction fetch)

--- src/datapath/datapath.v
+++ src/datapath/datapath.v
@@ -1,5 +1,45 @@
-module datapath #(parameter T = 32) (
-    input clk
+module datapath #(
+    parameter pcmux_N = 2;
+) (
+    input clk,
+    input [$clog2(pcmux_N)-1:0] pcsrc
 );
+    // -----------------------------------------------------------
+    // Regs
+    reg [31:0] pc;
+    // Wires
+    wire [31:0] pcadd4;
+    wire [31:0] pcnext;
+    wire [31:0] instr;
+    // -----------------------------------------------------------
+
+    // -----------------------------------------------------------
+    // logic for pcnext
+    add32 pc_adder (
+        .a(pc),
+        .b(32'd4),
+        .c_1(1'b0),
+        .s(pcadd4)
+    );
+    mux #(.W(32), .N(pcmux_N)) pc_mux (
+        .in({pcadd4}), // TODO: add other inputs for branch/jump
+        .sel(pcsrc), // (for now) pcsrc = 0 => pcadd4
+        .out(pcnext)
+    );
+    // pc update
+    always @(posedge clk) begin
+        pc <= pcnext;
+    end
+    // -----------------------------------------------------------
+
+    // -----------------------------------------------------------
+    // instruction fetch
+    instr_mem imem ( // simple instruction memory with clocked LUT
+        .a(pc),
+        .rd(instr),
+        // .re(instr_mem_re),
+        .clk(clk)
+    );
+    // -----------------------------------------------------------
 
 endmodule
--- src/datapath/mem_src/instr_mem.v
+++ src/datapath/mem_src/instr_mem.v
@@ -3,12 +3,15 @@
     Description: Simple, behavioral implementation of the L1 Instruction ROM (direct LUT)
  */
 module instr_mem #(parameter N = 1024)(
+    // input re, //  Read enable
+    input clk,
     input [$clog2(N)-1:0] a,
     output [31:0] rd
 );
     reg [31:0] instr [0:N-1];
-    always @(*)
+    always @(posedge clk)
     begin
-        rd <= instr[a];
+        // if (re)
+            rd <= instr[a];
     end
 endmodule
